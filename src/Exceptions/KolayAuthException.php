d9a5d287ea7b806211248cbf797fdedf0315f407
Serhat tan <serhat.tan@kolayik.com>
2018-07-20 00:32:20 +0300

    Add kolay auth exception get and set methods

--- src/Exceptions/KolayAuthException.php
+++ src/Exceptions/KolayAuthException.php
@@ -9,5 +9,27 @@ class KolayAuthException extends Exception
     /**
      * {@inheritdoc}
      */
+    public $details;
     protected $message = 'An error occurred';
+
+    /**
+     * @return mixed
+     */
+    public function getDetails()
+    {
+        return $this->details;
+    }
+
+    /**
+     * @param mixed $details
+     */
+    public function setDetails($details)
+    {
+        $this->details = $details;
+    }
+
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
 }
